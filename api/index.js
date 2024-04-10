c01a261bc220c5c63912ceb60074041cca17c0d4
Guilherme Gama <guilhermegamabs@gmail.com>
2024-04-10 14:49:19 -0300

    GetAll Users

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
-import express from "express";
+import express from 'express';
 import cors from 'cors';
+import userRoute from './routes/userRoute.js';
 
 const app = express();
-const PORT = 3333;
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT);
+app.use('/', userRoute);
+
+app.listen(3333);
